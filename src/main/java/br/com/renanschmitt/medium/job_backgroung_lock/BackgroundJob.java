1f157883a2e3c49ac7603e981ea95e612afcfc7b
Renan Schmitt <renanschmitt@gmail.com>
2025-11-06 22:57:48 +0000

    Introduce and refactor background job execution methods

--- src/main/java/br/com/renanschmitt/medium/job_backgroung_lock/BackgroundJob.java
+++ src/main/java/br/com/renanschmitt/medium/job_backgroung_lock/BackgroundJob.java
@@ -1,6 +1,7 @@
 package br.com.renanschmitt.medium.job_backgroung_lock;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.redisson.api.RLock;
@@ -18,28 +19,39 @@ public class BackgroundJob {
   private final RedissonClient redissonClient;
 
   //  @Scheduled(fixedDelay = 2000)
-  //  public void execute() {
-  //    Lock lock = redisLockRegistry.obtain("background-job-key");
-  //
-  //    if (lock.tryLock()) {
-  //      try {
-  //        log.info("Executing background job");
-  //        try {
-  //          Thread.sleep(1000);
-  //        } catch (InterruptedException e) {
-  //          log.error("Error executing background job", e);
-  //        }
-  //        log.info("Background job executed");
-  //      } finally {
-  //        lock.unlock();
-  //      }
-  //    } else {
-  //      log.warn("Could not acquire lock");
-  //    }
-  //  }
+  public void execute() {
+    log.info("Executing background job");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      log.error("Error executing background job", e);
+    }
+    log.info("Background job executed");
+  }
+
+  //  @Scheduled(fixedDelay = 2000)
+  public void execute2() {
+    Lock lock = redisLockRegistry.obtain("background-job-key");
+
+    if (lock.tryLock()) {
+      try {
+        log.info("Executing background job");
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          log.error("Error executing background job", e);
+        }
+        log.info("Background job executed");
+      } finally {
+        lock.unlock();
+      }
+    } else {
+      log.warn("Could not acquire lock");
+    }
+  }
 
   @Scheduled(fixedDelay = 2000)
-  public void execute() {
+  public void execute3() {
     RLock lock = redissonClient.getLock("background-job-key");
 
     try {
